2460d30208da7e35fff083c4725b13cc46f7f9c4
3dvkr <v3dvkr@gmail.com>
2025-03-17 13:18:38 -0600

    add cookies to test localhost:4000 in and out of iframe

--- app/index.js
+++ app/index.js
@@ -1,14 +1,34 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 const PORT = 4000;
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-// Middleware to allow embedding in iframes
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true })); // To handle form data
+
 app.use((req, res, next) => {
-    res.setHeader("X-Frame-Options", "ALLOWALL"); // Or 'SAMEORIGIN' if hosted together
+        res.setHeader("X-Frame-Options", "ALLOWALL"); // Or 'SAMEORIGIN' if hosted together
+    console.log(req.url, '\nCookies: ', req.cookies)
+    // if (req.path !== "/login" && req.cookies && req.cookies.authToken !== "secure123") {
+    //     return res.redirect("/login");
+    // }
+    if (req.path !== "/login" && req.path !== "/authenticate" && req.cookies.authToken !== "secure123") {
+        return res.redirect("/login");
+    }
     next();
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.post("/authenticate", (req, res) => {
+    res.cookie("authToken", "secure123", { path: "/", httpOnly: true, sameSite: "None", secure: true });
+    res.sendStatus(200);
 });
 
 // Home page inside the iframe
@@ -16,7 +36,7 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-// Another page inside the iframe
+// Second page inside the iframe
 app.get("/page2", (req, res) => {
     res.render("page2");
 });
